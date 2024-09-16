780a8e85ae72cc73713eb8a6d298432bbc7eb961
grass2 <cuongdieu@gmail.com>
2024-09-16 19:49:57 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -70,18 +70,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ese_weaver
 
-# otacert
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/magisk \
-    $(DEVICE_PATH)/security/miui_releasekey \
-
-# PRODUCT_RELEASE_NAME ro.twrp.device.name
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
-
-TWRP_REQUIRED_MODULES += \
-    miui_prebuilt \
-    magisk_prebuilt \
-
-ifneq ($(TW_SKKK_VER_CODE),)
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.version.skkk.code=$(TW_SKKK_VER_CODE)
-endif
